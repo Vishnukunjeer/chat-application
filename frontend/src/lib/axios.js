9fadac532a100cbefcb601a35e16d932b7803e2b
Vishnukunjeer <101510914+Vishnukunjeer@users.noreply.github.com>
2025-05-31 12:26:35 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://doctor-booking-application-backend.onrender.com",
+  baseURL: "development"
+      ? "http://localhost:5001/api"
+      : "https://doctor-booking-application-backend.onrender.com/api",
   withCredentials: true,
 });
