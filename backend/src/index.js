931fac00f9d9ebe59dffce9047cc6a14cf8fb72e
Rajat Kunjeer <kunjeervishnu6@gmail.com>
2026-02-16 16:57:18 +0530

    backup

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,14 +13,28 @@ import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
 const __dirname = path.resolve();
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://chat-application-kappa-steel.vercel.app",
+  "https://chat-application-olt2738if-vishnu-kunjirs-projects.vercel.app",
+];
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://chat-application-kappa-steel.vercel.app"],
+    origin: function (origin, callback) {
+      // allow requests with no origin (like curl, Postman)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
     credentials: true,
   })
 );
