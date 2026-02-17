0264c58fe74c4c9f05f1647e120205b0eaf9c984
Rajat Kunjeer <kunjeervishnu6@gmail.com>
2026-02-17 16:29:16 +0530

    1

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,7 @@ import path from "path";
 
 import { connectDB } from "./lib/db.js";
 
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
@@ -18,8 +19,8 @@ const PORT = process.env.PORT || 5001;
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://chat-application-kappa-steel.vercel.app",
-  "https://chat-application-olt2738if-vishnu-kunjirs-projects.vercel.app",
+  "https://chat-application-1-bjcd.onrender.com",
+  "https://chat-application-5svg.onrender.com",
 ];
 
 app.use(express.json());
@@ -50,6 +51,11 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
+import { io } from "socket.io-client";
+
+const socket = io("https://chat-application-5svg.onrender.com", {
+  withCredentials: true, // allow sending cookies
+});
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
